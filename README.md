c16dc4818c24d476bf53b58e1d211528a403cbcd
cheejiayuan512 <cheejiayuan@yahoo.com.sg>
2022-08-11 15:35:49 +0800

    FBI-6 initial creation of flask api with swagger ui and template swagdefs

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FlaskAPIBackend
 API and cloud DB for Deep Learning models as well as endpoints for Raspberry Pies to upload data
+
+#Currently OpenAPI 2.0
+#Future Plans
+OAuth2 integration
+
+Make Dockerfile
+
+Deploy Containerized Flask app to Heroku
+
+Testing
+
+WebSocket streaming
---
+++ main.py
@@ -0,0 +1,66 @@
+from flask import Flask, jsonify
+from flasgger import Swagger, swag_from
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+
+@app.route('/colors/<palette>/')
+def colors(palette):
+    """Example endpoint returning a list of colors by palette
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: palette
+        in: path
+        type: string
+        enum: ['all', 'rgb', 'cmyk']
+        required: true
+        default: all
+    definitions:
+      Palette:
+        type: object
+        properties:
+          palette_name:
+            type: array
+            items:
+              $ref: '#/definitions/Color'
+      Color:
+        type: string
+    responses:
+      200:
+        description: A list of colors (may be filtered by palette)
+        schema:
+          $ref: '#/definitions/Palette'
+        examples:
+          rgb: ['red', 'green', 'blue']
+    """
+    all_colors = {
+        'cmyk': ['cyan', 'magenta', 'yellow', 'black'],
+        'rgb': ['red', 'green', 'blue']
+    }
+    if palette == 'all':
+        result = all_colors
+    else:
+        result = {palette: all_colors.get(palette)}
+
+    return jsonify(result)
+
+
+@app.route('/coordinates/', methods=['GET', 'POST', 'DELETE'])
+@swag_from('swagger_defs/coordinates_get.yaml', methods=['GET'])
+@swag_from('swagger_defs/coordinates_post.yaml', methods=['POST'])
+def coordinates():
+    all_colors = {
+        'cmyk': ['cyan', 'magenta', 'yellow', 'black'],
+        'rgb': ['red', 'green', 'blue']
+    }
+    if palette == 'all':
+        result = all_colors
+    else:
+        result = {palette: all_colors.get(palette)}
+
+    return jsonify(result)
+
+
+app.run(debug=True)
